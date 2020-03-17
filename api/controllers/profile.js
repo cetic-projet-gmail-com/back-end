7985267ccdfbff3bb36075acf5193268ec52c29b
elian <caby.elian01@gmail.com>
2020-03-17 23:46:51 +0100

    patch profile

--- api/controllers/profile.js
+++ api/controllers/profile.js
@@ -23,37 +23,44 @@ exports.find = async (req, res) => {
 exports.update = async (req, res) => {
     let { id } = req.payload;
     let updatedUser = req.body;
-
+    console.log(updatedUser)
     await User
         .update({
-            login: updatedUser.login ? updatedUser.login : undefined,
-            firstName: updatedUser.firstName ? updatedUser.firstName : undefined,
-            lastName: updatedUser.lastName ? updatedUser.lastName : undefined,
-            password: updatedUser.password ? updatedUser.password : undefined,
-            roleId: updatedUser.roleId ? updatedUser.roleId : undefined,
-            email: updatedUser.email ? updatedUser.email : undefined,
-            departmentId: updatedUser.departmentId ? updatedUser.departmentId : undefined
+            // login: updatedUser.login ? updatedUser.login : undefined,
+            // firstName: updatedUser.firstName ? updatedUser.firstName : undefined,
+            // lastName: updatedUser.lastName ? updatedUser.lastName : undefined,
+            // password: updatedUser.password ? updatedUser.password : undefined,
+            // roleId: updatedUser.roleId ? updatedUser.roleId : undefined,
+            // email: updatedUser.email ? updatedUser.email : undefined,
+            // departmentId: updatedUser.departmentId ? updatedUser.departmentId : undefined
+            email : updatedUser.email,
+            password: updatedUser.password
         },
             {
                 where: { id: id }
+            }).then(() => {
+                return res.status(200).json({"infos": "profil mis à jour"})
             })
         .catch((err) => {
+            return res.status(422).json({"infos": "errors"})
+
             console.log(`The following error has occured: ${err}`);
         })
 
-    let user = await User
-        .findOne({
-            where: { id: id },
-            include: ['role', 'department','']
-        })
-        .then((user) => {
-            if (user) {
-                user.password = undefined
-                return user;
-            }
-        })
-        .catch((err) => {
-            console.log(`The following error has occured: ${err}`);
-        })
-    res.status(200).json({ user });
+    // let user = await User
+    //     .findOne({
+    //         where: { id: id },
+    //         include: ['role', 'department','']
+    //     })
+    //     .then((user) => {
+    //         if (user) {
+    //             user.password = undefined
+    //             return user;
+    //         }
+    //     })
+    //     .catch((err) => {
+    //         return res.status(422)
+    //         console.log(`The following error has occured: ${err}`);
+    //     })
+    // res.status(200).json({ user });
 }
