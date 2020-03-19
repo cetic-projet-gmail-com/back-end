f92b002c23079597df5840f290bf6bfa175c1e30
Anis Toumi <t.anis@hotmail.be>
2020-03-19 03:51:34 +0100

    sal script update

--- sql/dbscript.sql
+++ sql/dbscript.sql
@@ -65,7 +65,7 @@ CREATE TABLE departments (
 DROP TABLE IF EXISTS events;
 CREATE TABLE events (
   id int(11) NOT NULL AUTO_INCREMENT,
-  userId int(11) NOT NULL DEFAULT 0,
+  userId int(11) DEFAULT NULL,
   taskId int(11) DEFAULT NULL,
   description text COLLATE utf8_unicode_ci,
   duration float DEFAULT NULL,
@@ -88,7 +88,6 @@ CREATE TABLE users (
   createdAt datetime DEFAULT NULL,
   updatedAt datetime DEFAULT NULL,
   roleId int(11)  DEFAULT NULL,
-  archived tinyint(1)  DEFAULT 0,
   email varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `password` varchar(128) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   PRIMARY KEY (id),
@@ -118,6 +117,7 @@ CREATE TABLE roles (
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+
 ALTER TABLE tasks ADD CONSTRAINT tasks_ibfk_1 FOREIGN KEY (activityId) REFERENCES activities (id);
 ALTER TABLE activitiesAssignments ADD CONSTRAINT activity_assignments_ibfk_2 FOREIGN KEY (activityId) REFERENCES activities (id);
 ALTER TABLE activitiesAssignments ADD CONSTRAINT activity_assignments_ibfk_1 FOREIGN KEY (userId) REFERENCES users (id);
@@ -139,24 +139,28 @@ INSERT INTO users (departmentId , email , firstName , lastName , roleId , create
 INSERT INTO users (departmentId , email , firstName , lastName , roleId , createdAt , updatedAt) VALUES (1, 'emailtest@test.test', 'Test', 'TEST', 2, '2020-03-02', '2020-03-02');
 INSERT INTO users (departmentId , email , firstName , lastName , roleId , createdAt , updatedAt) VALUES (1, 'emaiergltest@test.test', 'Teergst', 'TEergergST', 2, '2020-03-02', '2020-03-02');
 UPDATE departments SET departments.responsibleId = 1 WHERE departments.id = 1;
-INSERT INTO colours (name , code ) VALUES ('Vert Foncé', '#53853d'), ('Vert CETIC', '#6fb052'), ('Vert clair', '#d7e6cc'), ('Gris Foncé', '#5a5a5a'), ('Gris Moyen', '#808080'), ('Gris clair', '#cccccc'), ('Rouge ECS', '#f62f00'), ('Jaune SSE', '#feb602'), ('Bleu SST', '#00b0f0'), ('Bleu foncé', '#0070c0'), ('Mauve', '#7030a0');
+INSERT INTO colours (name , code ) VALUES ('Vert Fonce', '#53853d'), ('Vert CETIC', '#6fb052'), ('Vert clair', '#d7e6cc'), ('Gris Fonce', '#5a5a5a'), ('Gris Moyen', '#808080'), ('Gris clair', '#cccccc'), ('Rouge ECS', '#f62f00'), ('Jaune SSE', '#feb602'), ('Bleu SST', '#00b0f0'), ('Bleu fonce', '#0070c0'), ('Mauve', '#7030a0');
 INSERT INTO activities (aTypeId , colourId , createdAt, updatedAt , description , ended , name , projectManagerId ) VALUES (1, 2, '2020-03-02','2020-03-02' , 'on fait des tests', 0, 'Tests', 2);
 INSERT INTO activities (aTypeId , colourId , createdAt, updatedAt , description , ended , name , projectManagerId ) VALUES (1, 2, '2020-03-02','2020-03-02' , 'on fait des tests', 0, 'ergerg', 2);
 INSERT INTO activities (aTypeId , colourId , createdAt, updatedAt , description , ended , name , projectManagerId ) VALUES (1, 2, '2020-03-02','2020-03-02' , 'on fawergests', 0, 'ergerg', 2);
 INSERT INTO activities (aTypeId , colourId , createdAt, updatedAt , description , ended , name , projectManagerId ) VALUES (1, 2, '2020-03-02','2020-03-02' , 'on fawerg tests', 0, 'erg', 2);
 INSERT INTO activities (aTypeId , colourId , createdAt, updatedAt , description , ended , name , projectManagerId ) VALUES (1, 2, '2020-03-02','2020-03-02' , 'on faergtests', 0, 'reg', 2);
 INSERT INTO tasks (activityId, ended, createdAt, updatedAt , name, description ) VALUES (1, 0, '2020-03-02',  '2020-03-02', 'tache test', 'ceci est un petit test');
-INSERT INTO tasks (activityId, ended, createdAt, updatedAt , name, description ) VALUES (1, 0, '2020-03-03',  '2020-03-04', 'tafwe test 2', 'ceci est wefwef petit test');
-INSERT INTO tasks (activityId, ended, createdAt, updatedAt , name, description ) VALUES (1, 0, '2020-03-03',  '2020-03-04', 'tacwefwefwfest 2', 'ceci est un autre petit test');
-INSERT INTO tasks (activityId, ended, createdAt, updatedAt , name, description ) VALUES (1, 0, '2020-03-03',  '2020-03-04', 'tacwefst 2', 'ceci est un auwefwefit test');
+INSERT INTO tasks (activityId, ended, createdAt, updatedAt , name, description ) VALUES (2, 0, '2020-03-03',  '2020-03-04', 'tafwe test 2', 'ceci est wefwef petit test');
+INSERT INTO tasks (activityId, ended, createdAt, updatedAt , name, description ) VALUES (5, 0, '2020-03-03',  '2020-03-04', 'tacwefwefwfest 2', 'ceci est un autre petit test');
+INSERT INTO tasks (activityId, ended, createdAt, updatedAt , name, description ) VALUES (3, 0, '2020-03-03',  '2020-03-04', 'tacwefst 2', 'ceci est un auwefwefit test');
 INSERT INTO tasks (activityId, ended, createdAt, updatedAt , name, description ) VALUES (1, 0, '2020-03-03',  '2020-03-04', 'tacwefwefwef 2', 'ceci eswefwutre petit test');
 INSERT INTO tasksAssignments (taskId , userId, createdAt , updatedAt ) VALUES (1, 1, '2020-03-02', '2020-03-02');
+INSERT INTO tasksAssignments (taskId , userId, createdAt , updatedAt ) VALUES (2, 3, '2020-03-02', '2020-03-02');
+INSERT INTO tasksAssignments (taskId , userId, createdAt , updatedAt ) VALUES (3, 1, '2020-03-02', '2020-03-02');
+INSERT INTO tasksAssignments (taskId , userId, createdAt , updatedAt ) VALUES (4, 3, '2020-03-02', '2020-03-02');
+INSERT INTO tasksAssignments (taskId , userId, createdAt , updatedAt ) VALUES (5, 2, '2020-03-02', '2020-03-02');
 INSERT INTO activitiesAssignments (activityId , userId, createdAt , updatedAt ) VALUES (1, 1, '2020-03-02', '2020-03-02');
-INSERT INTO activitiesAssignments (activityId , userId, createdAt , updatedAt ) VALUES (2, 3, '2020-03-02', '2020-03-02');
+INSERT INTO activitiesAssignments (activityId , userId, createdAt , updatedAt ) VALUES (2, 2, '2020-03-02', '2020-03-02');
 INSERT INTO activitiesAssignments (activityId , userId, createdAt , updatedAt ) VALUES (3, 3, '2020-03-02', '2020-03-02');
 INSERT INTO activitiesAssignments (activityId , userId, createdAt , updatedAt ) VALUES (4, 3, '2020-03-02', '2020-03-02');
 INSERT INTO activitiesAssignments (activityId , userId, createdAt , updatedAt ) VALUES (5, 1, '2020-03-02', '2020-03-02');
-INSERT INTO events (taskId , userId , description , startAt, endAt, duration , createdAt , updatedAt ) VALUES (1, 1, 'la description 1', '2020-03-03 8:30:00', '2020-03-03 10:30:00', 2, '2020-03-02', '2020-03-02'), (1, 1, 'la description 1', '2020-03-03 11:00:00', '2020-03-03 13:00:00', 2, '2020-03-02', '2020-03-02'), (1, 1, 'la description 1', '2020-03-03 13:30:00', '2020-03-03 15:00:00', 2, '2020-03-02', '2020-03-02');
+INSERT INTO events (taskId , userId , description , startAt, endAt, duration , createdAt , updatedAt ) VALUES (1, 1, 'la description 1', '2020-03-03 8:30:00', '2020-03-03 10:30:00', 2, '2020-03-02', '2020-03-02'), (4, 1, 'la description 1', '2020-03-03 11:00:00', '2020-03-03 13:00:00', 2, '2020-03-02', '2020-03-02'), (1, 2, 'la description 1', '2020-03-03 13:30:00', '2020-03-03 15:00:00', 2, '2020-03-02', '2020-03-02'), (1, 3, 'la description 1', '2020-03-03 13:30:00', '2020-03-03 15:00:00', 2, '2020-03-02', '2020-03-02'), (5, 3, 'la description 1', '2020-03-03 13:30:00', '2020-03-03 15:00:00', 2, '2020-03-02', '2020-03-02');
 
 
 
