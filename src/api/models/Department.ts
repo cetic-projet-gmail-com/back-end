82c4e9be0034084e702319ccefd32458ff1bdc7a
ElianC <54775334+ElianC@users.noreply.github.com>
2020-12-29 22:45:26 +0100

    wip

--- src/api/models/Department.ts
+++ src/api/models/Department.ts
@@ -1,12 +1,11 @@
-/* import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm'
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm'
 import { User } from './User'
 
 @Entity()
-export class Department extends BaseEntity {
+export class Department {
   @PrimaryGeneratedColumn()
   id: number
 
   @ManyToOne(() => User, (user) => user.departments)
   user: User
 }
-*/
