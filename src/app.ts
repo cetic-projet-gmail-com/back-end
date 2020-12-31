fd726ff3af45956884a4175422134bee89f23e5b
ElianC <54775334+ElianC@users.noreply.github.com>
2020-12-31 13:21:40 +0100

    add cors origin

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express from 'express'
+import cors from 'cors'
 
 import MainRouter from './api/routes/index'
 
 const app = express()
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }))
+
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+}))
 
 app.all('*', (req, res, next) => {
   console.log(`${req.method} ${req.originalUrl}`)
