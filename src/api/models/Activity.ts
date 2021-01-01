42bf921a139d3e1a81dc1524fc64f739e809d0bf
ElianC <54775334+ElianC@users.noreply.github.com>
2021-01-01 21:19:18 +0100

    Create Activity.ts

---
+++ src/api/models/Activity.ts
@@ -0,0 +1,42 @@
+
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, CreateDateColumn, UpdateDateColumn, ManyToOne } from 'typeorm'
+
+import { Task } from './Task'
+import { User } from './User'
+
+@Entity()
+export class Activity {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({
+    nullable: false,
+    unique: false,
+  })
+  name: string
+
+  @Column({
+    nullable: false,
+    unique: false,
+  })
+  description: string
+
+  @Column()
+  ended: boolean = false
+
+  @CreateDateColumn()
+  createdAt: Date
+
+  @UpdateDateColumn()
+  updatedAt: Date
+
+  @OneToMany(type => Task, tasks => tasks.activity, {
+    cascade: true,
+  })
+  tasks: Task[]
+
+  @ManyToOne(type => User)
+  projectManager: User
+
+  //TODO crmId, colour, aType
+}
