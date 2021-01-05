4dd8e8666e295723658df5fce1363a1a6e7c3f1b
ElianC <54775334+ElianC@users.noreply.github.com>
2021-01-05 21:04:54 +0100

    Update auth.ts

--- src/api/controllers/auth.ts
+++ src/api/controllers/auth.ts
@@ -1,7 +1,7 @@
 import { getRepository } from 'typeorm'
 import jwt from 'jsonwebtoken'
 
-import User from '../models/User'
+import User from '../models/User.js'
 
 export const login = async (req, res, next) => {
   try {
